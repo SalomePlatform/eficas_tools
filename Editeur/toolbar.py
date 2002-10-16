da97b56c3e83c4bbc6c4acb57bad7baddddce9c5
eficas <>
2002-10-16 10:25:43 +0000

    CCAR : remplacement separateur New24.gif par Sep.gif suppression de la methode creer_boutons plus utilisee

--- Editeur/toolbar.py
+++ Editeur/toolbar.py
@@ -46,52 +46,6 @@ class TOOLBAR:
       b.bind("<Enter>",lambda e,s=self,but=b,t=texte : s.affiche_balloon(e,but,t,pos='right'))
       b.bind("<Leave>", self.efface_balloon)
 
-      #self.creer_boutons()
-
-  def creer_boutons(self):
-      self.l_boutons = (('New24',self.appli.newJDC,"Création d'un nouveau fichier",'always'),
-                        ('Open24',self.appli.openJDC,"Ouverture d'un fichier existant",'always'),
-                        ('Save24',self.appli.saveJDC,"Sauvegarde du fichier courant",'always'),
-                        ('Zoom24',self.appli.visuJDC,"Visualisation du fichier de commandes",'always'),
-                        None,
-                        ('Copy24',self.appli.copy,"Copie l'objet courant",'jdc'),
-                        ('Cut24',self.appli.cut,"Coupe l'objet courant",'jdc'),
-                        ('Paste24',self.appli.paste,"Colle l'objet copié après l'objet courant",'jdc'),
-                        None,
-                        ('Delete24',self.appli.delete,"Supprime l'objet courant",'jdc'),
-                        ('Help24',self.appli.view_doc,"Documentation de l'objet courant",'jdc')
-                        )
-      # liste des boutons à activer quand statut != 'always'
-      self.l_boutons_a_activer = [] 
-
-      for bouton in self.l_boutons :
-          if not bouton :
-              # on veut afficher un bouton vide (=espace entre boutons)
-              Button(self.barreboutons,
-                     image = images.get_image('New24'),
-                     relief = 'flat').pack(side='left')
-              continue
-          nom_fic,commande,texte,statut = bouton
-          b = Button(self.barreboutons,
-                     image = images.get_image(nom_fic),
-                     command = commande,
-                     relief='flat')
-          b.pack(side='left')
-          b.bind("<Enter>",lambda e,s=self,but=b,t=texte : s.affiche_balloon(e,but,t))
-          b.bind("<Leave>", self.efface_balloon)
-          if statut != 'always':
-              self.l_boutons_a_activer.append(b)
-      # bouton Infos à l'extrême droite de la barre des boutons
-      b = Button(self.barreboutons,
-                 image = images.get_image('About24'),
-                 command = self.view_infos)
-      b.pack(side='right')
-      texte = "Infos EFICAS"
-      b.bind("<Enter>",lambda e,s=self,but=b,t=texte : s.affiche_balloon(e,but,t,pos='right'))
-      b.bind("<Leave>", self.efface_balloon)
-      # inactive les boutons qui doivent l'être tant qu'aucun JDC courant
-      self.inactive_boutons()
-
   def inactive_boutons(self):
       """
       Inactive les boutons de la liste self.l_boutons_a_activer
@@ -161,7 +115,8 @@ class TOOLBAR:
           if not bouton :
               # on veut afficher un bouton vide (=espace entre boutons)
               Button(self.barreboutons,
-                     image = images.get_image('New24'),
+                     image = images.get_image('Sep'),
+                     state='disabled',
                      relief = 'flat').pack(side='left')
               continue
           nom_fic,commande,texte,statut = bouton
