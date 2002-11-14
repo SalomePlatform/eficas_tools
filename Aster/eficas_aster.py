2890e7b8087a5b6d16c16a9f8533082aace807ae
eficas <>
2002-11-14 13:08:57 +0000

    retirer Trace

--- Aster/eficas_aster.py
+++ Aster/eficas_aster.py
@@ -29,16 +29,12 @@ import sys
 import prefs
 sys.path[:0]=[prefs.INSTALLDIR]
 
-import Misc.Trace
 import Editeur
 from Editeur import eficas_go
 
-#Misc.Trace.begin_trace()
 if len(sys.argv) > 1 :
     # on veut ouvrir un fichier directement au lancement d'Eficas
     eficas_go.lance_eficas(code='ASTER',fichier = sys.argv[1])
 else:
     # on veut ouvrir Eficas 'vide'
     eficas_go.lance_eficas(code='ASTER')
-Misc.Trace.end_trace()
-
