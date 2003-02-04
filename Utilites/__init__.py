6f12ab26d3fcef2cf199913ad61513e82b07cf58
eficas <>
2003-02-04 10:40:57 +0000

    Ajout des mots-clés RCS

--- Utilites/__init__.py
+++ Utilites/__init__.py
@@ -1,10 +1,13 @@
 """
+    $Id$
+
     Le package UTILITES contient les fonctions permettant
     d'instrumenter un script :
     - MESSAGE (module message)
     - SCRUTE (module scrute)
     - PAUSE (module pause)
 """
+
 
 __all__ = [ "ici" , "message" , "scrute" , "pause" ]
 from pause import *
