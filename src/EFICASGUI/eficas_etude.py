eff9f4e8a93ea66d112ee6c74ed069fc9870ded8
salome <>
2004-11-15 17:46:46 +0000

    Ajout du fichier eficas_etude. Il importe eficasEtude et sert uniquement à assurer la continuité entre versions de Eficas.

---
+++ src/EFICASGUI/eficas_etude.py
@@ -0,0 +1,11 @@
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# CE FICHIER EST CREE UNIQUEMENT POUR ASSURER LA COMPATIBILITE
+# ASCENDANTE DES VERSIONS DE EFICAS. NE PAS EDITER, MODIFIER eficasEtude.
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# En version 1.5 et 1.6 de Eficas, l'implémentation du couplage avec
+# SALOME comprend l'import d'un fichier nommé eficas_etude. En version
+# préparatoire de la version 1.7, cette implémentation comprend l'import
+# d'un fichier eficasEtude. A terme, seul le fichier eficasEtude restera.
+# _CS_gbo_151104
+
+import eficasEtude
