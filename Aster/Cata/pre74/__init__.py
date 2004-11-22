750e6cd57cb8300a5a8988cd195443204ca76a5c
eficas <>
2004-11-22 10:10:27 +0000

    PN : pour permettre pour les anciennes versions ( aster 5 et aster 6)      l affichage des parametres eval apres les formules et les commentaires.

--- Aster/Cata/pre74/__init__.py
+++ Aster/Cata/pre74/__init__.py
@@ -4,5 +4,5 @@ from FORM import FORM,FORM_ETAPE
 Accas.FORM=FORM
 Accas.FORM_ETAPE=FORM_ETAPE
 
-import compooper,compoproc,compomacro
+import compooper,compoproc,compomacro,compocomm,compoparam
 import compoformule
