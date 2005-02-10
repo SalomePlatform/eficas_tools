7507cca37d6644d69f1cfebb290394693d41ee61
salome <>
2005-02-10 15:34:45 +0000

    Pour aster 7.3

--- src/EFICASGUI/dataEficas.py
+++ src/EFICASGUI/dataEficas.py
@@ -5,15 +5,16 @@ eficas_root=os.environ["EFICAS_ROOT"]
 sys.path[:0]=[os.path.join(eficas_root,'Aster'),
               os.path.join(eficas_root,'Homard'),
               eficas_root,
-              os.path.join(eficas_root,'Editeur'),
+#             os.path.join(eficas_root,'Editeur'),
              ]
 
 # Modules Python
 import Tkinter
 
 # Modules Eficas
-import import_code
-import splash
+#import import_code
+from Editeur import splash
+from Editeur import eficas
 
 
 def init(top,code="ASTER",fichier=None):
@@ -23,7 +24,7 @@ def init(top,code="ASTER",fichier=None):
     import eficasEtude
     MaRef=eficasEtude.Eficas_In_Study(code)
 
-    import eficas
+    #import eficas
     class MyEficas(eficas.EFICAS):
         def quit(self):
             eficas.EFICAS.quit(self)
