730bbe8ecf032cf55a09d37e86820cb255be2584
eficas <>
2005-09-19 17:39:39 +0000

    correction chemin doc

--- Editeur/bureau.py
+++ Editeur/bureau.py
@@ -629,8 +629,7 @@ class BUREAU:
           cle_doc = string.replace(cle_doc,'-','')
           commande = self.appli.CONFIGURATION.exec_acrobat
           nom_fichier = cle_doc+".pdf"
-          rep_fichier = cle_doc[0:2]
-          fichier = os.path.abspath(os.path.join(self.appli.CONFIGURATION.path_doc,rep_fichier,nom_fichier))
+          fichier = os.path.abspath(os.path.join(self.appli.CONFIGURATION.path_doc,nom_fichier))
           if os.name == 'nt':
               os.spawnv(os.P_NOWAIT,commande,(commande,fichier,))
           elif os.name == 'posix':
