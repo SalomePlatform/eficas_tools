284e57e3cbf830993613bc289b27ff100fbedc2a
Pascale Noyret <pascale.noyret@edf.fr>
2010-10-05 06:55:55 +0000

    *** empty log message ***

--- UiQT4/desListeParam.py
+++ UiQT4/desListeParam.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'desListeParam.ui'
 #
-# Created: Tue Jan 27 12:25:36 2009
-#      by: PyQt4 UI code generator 4.4.3
+# Created: Fri Sep 24 15:14:55 2010
+#      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,17 +12,30 @@ from PyQt4 import QtCore, QtGui
 class Ui_DLisParam(object):
     def setupUi(self, DLisParam):
         DLisParam.setObjectName("DLisParam")
-        DLisParam.resize(413, 394)
-        self.gridlayout = QtGui.QGridLayout(DLisParam)
-        self.gridlayout.setObjectName("gridlayout")
+        DLisParam.resize(420, 425)
+        self.gridLayout = QtGui.QGridLayout(DLisParam)
+        self.gridLayout.setObjectName("gridLayout")
         self.LBParam = QtGui.QListWidget(DLisParam)
+        self.LBParam.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
         self.LBParam.setObjectName("LBParam")
-        self.gridlayout.addWidget(self.LBParam, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.LBParam, 0, 0, 1, 1)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtGui.QSpacerItem(128, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.BOk = QtGui.QPushButton(DLisParam)
+        self.BOk.setObjectName("BOk")
+        self.horizontalLayout.addWidget(self.BOk)
+        spacerItem1 = QtGui.QSpacerItem(168, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
 
         self.retranslateUi(DLisParam)
+        QtCore.QMetaObject.connectSlotsByName(DLisParam)
 
     def retranslateUi(self, DLisParam):
         DLisParam.setWindowTitle(QtGui.QApplication.translate("DLisParam", "Sélection de paramétres", None, QtGui.QApplication.UnicodeUTF8))
+        self.BOk.setText(QtGui.QApplication.translate("DLisParam", "Valider", None, QtGui.QApplication.UnicodeUTF8))
 
 
 if __name__ == "__main__":
