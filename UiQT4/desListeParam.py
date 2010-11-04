c9c6a7c9a0cf2a636536ef00f38d8455a9afca14
Pascale Noyret <pascale.noyret@edf.fr>
2010-11-04 13:39:38 +0000

    *** empty log message ***

--- UiQT4/desListeParam.py
+++ UiQT4/desListeParam.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'desListeParam.ui'
 #
-# Created: Fri Sep 24 15:14:55 2010
-#      by: PyQt4 UI code generator 4.7.3
+# Created: Tue Nov  2 16:22:59 2010
+#      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,23 +12,23 @@ from PyQt4 import QtCore, QtGui
 class Ui_DLisParam(object):
     def setupUi(self, DLisParam):
         DLisParam.setObjectName("DLisParam")
-        DLisParam.resize(420, 425)
+        DLisParam.resize(420,425)
         self.gridLayout = QtGui.QGridLayout(DLisParam)
         self.gridLayout.setObjectName("gridLayout")
         self.LBParam = QtGui.QListWidget(DLisParam)
         self.LBParam.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
         self.LBParam.setObjectName("LBParam")
-        self.gridLayout.addWidget(self.LBParam, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.LBParam,0,0,1,1)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtGui.QSpacerItem(128, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem = QtGui.QSpacerItem(128,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
         self.BOk = QtGui.QPushButton(DLisParam)
         self.BOk.setObjectName("BOk")
         self.horizontalLayout.addWidget(self.BOk)
-        spacerItem1 = QtGui.QSpacerItem(168, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem1 = QtGui.QSpacerItem(168,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
-        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout,1,0,1,1)
 
         self.retranslateUi(DLisParam)
         QtCore.QMetaObject.connectSlotsByName(DLisParam)
