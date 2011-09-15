44d5e6aa2636133b314e573c20a27a2774a14703
Pascale Noyret <pascale.noyret@edf.fr>
2011-09-15 08:00:05 +0000

    refonte des prefs pour le multicatalogue

--- Tests/prefs.py
+++ Tests/prefs.py
@@ -21,14 +21,13 @@
 
 import os,sys
 
-# REPINI sert à localiser le fichier editeur.ini
+# repIni sert à localiser le fichier editeur.ini
 # Obligatoire
-REPINI=os.path.dirname(os.path.abspath(__file__))
-repIni=REPINI
+repIni=os.path.dirname(os.path.abspath(__file__))
 
 # INSTALLDIR sert à localiser l'installation d'Eficas
 # Obligatoire
-INSTALLDIR=os.path.join(REPINI,'..')
+INSTALLDIR=os.path.join(repIni,'..')
 
 # CODE_PATH sert à localiser Noyau et Validation éventuellement
 # non contenus dans la distribution EFICAS
