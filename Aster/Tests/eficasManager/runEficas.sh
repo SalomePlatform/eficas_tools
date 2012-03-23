53ab7351d513ecc726c2400239fff09bc6efc712
gboulant <>
2012-03-23 18:17:06 +0000

    Documentation on eficasManager.py

--- Aster/Tests/eficasManager/runEficas.sh
+++ Aster/Tests/eficasManager/runEficas.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# This run the test with the good environment (no previous extention is required)
+# (gboulant - 23/03/2012)
 here=$(dirname $0)
 EFICAS_ROOT=$here/../../..
 export PYTHONPATH=$EFICAS_ROOT/Aster:$EFICAS_ROOT:$PYTHONPATH
-
 python $here/usecase.py
--- Aster/Tests/eficasManager/usecase.py
+++ Aster/Tests/eficasManager/usecase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
+# This illustrates how to run Eficas dynamically with a user defined catalog
+# (gboulant - 23/03/2011)
 
 import eficasManager
 eficasManager.addCatalog(catalogName="demo", catalogPath="mycata.py")
