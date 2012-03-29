9aad820545a0d5f510e5ad6057018ff0b0ee6a79
Pascale Noyret <pascale.noyret@edf.fr>
2012-03-29 14:51:39 +0000

    *** empty log message ***

--- MAP/map_cata.py
+++ MAP/map_cata.py
@@ -22,8 +22,9 @@ v=SIMP(typ='TXM',fr='',ang='',statut='o',docu='',into=['a', 'b'],min=1,max=1,def
                     )
 
 S_TEST01_DATA=PROC(nom='S_TEST01_DATA',op=None,
-y=SIMP(typ='R',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
-x=SIMP(typ='R',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
-z=SIMP(typ='I',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
+a_string=SIMP(typ='TXM',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
+an_integer=SIMP(typ='I',fr='',ang='',statut='o',docu='',into=None,min=1,max=1000,val_min='**',val_max='**',defaut=None),
+a_float=SIMP(typ='R',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
+file_output=SIMP(typ='TXM',fr='',ang='',statut='o',docu='',into=None,min=1,max=1,val_min='**',val_max='**',defaut=None),
 )
 
