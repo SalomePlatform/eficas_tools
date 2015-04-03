3fc65fa71ec03020ae328dd05afe7bc43f0c59c4
pascale.noyret <pascale.noyret@edf.fr>
2015-04-03 15:56:15 +0200

    10000 en 50000

--- CarmelCND/CarmelCND_Cata.py
+++ CarmelCND/CarmelCND_Cata.py
@@ -15,33 +15,43 @@ class vcut(ASSD):
 class zs(ASSD):
    pass
 
+
+import types
+class Tuple:
+  def __init__(self,ntuple):
+    self.ntuple=ntuple
+
+  def __convert__(self,valeur):
+    if type(valeur) == types.StringType:
+      return None
+    if len(valeur) != self.ntuple:
+      return None
+    return valeur
+
+  def info(self):
+    return "Tuple de %s elements" % self.ntuple
+
+  __repr__=info
+  __str__=info
+
 JdC = JDC_CATA (code = 'monCode',
                 execmodul = None,
                )
                 
 # ======================================================================
-# ======================================================================
-#INCLUDE = MACRO ( nom = "INCLUDE", op = None,
-#DONNEES_GENE=MACRO(nom='DONNEES_GENE',op=None,
-#                UIinfo = { "groupes" : ( "iii", ) },
-#                sd_prod = opsCarmelCND.INCLUDE,
-#                fichier_ini = 1,
-
-#          mesh_file_name=SIMP(typ=('Fichier', 'All Files (*.med)'),fr= "No comment",ang= "No comment",statut= "o",),
-#)
 
 
 SOURCE=OPER(nom='SOURCE',op=None,sd_prod=source,UIinfo = { "groupes" : ( "CACHE", ) },
             EnveloppeConnexeInducteur=SIMP(statut='o',typ='TXM',defaut="default"),
-            VecteurDirecteur=SIMP(statut='o',typ='R',min=3,max=3,defaut=(0,0,1)),
-            Centre=SIMP(statut='o',typ='R',min=3,max=3),
-            SectionBobine=SIMP(statut='o',typ='R',fr='en m2',ang='en m2'),
-            Amplitude=SIMP(statut='o',typ='R',fr='en A',ang='en A'),
+            VecteurDirecteur=SIMP(statut='i',typ=Tuple(3),defaut=(0,0,1),validators=VerifTypeTuple(('R','R','R'))),
+            Centre=SIMP(statut='o',typ=Tuple(3),validators=VerifTypeTuple(('R','R','R'))),
+            SectionBobine=SIMP(statut='o',typ='R',fr='en m2',ang='(m2)'),
+            Amplitude=SIMP(statut='o',typ='R',fr='en A',ang='(A)'),
             NbdeTours=SIMP(statut='o',typ='I',val_min=1),
 )
 
 CONDUCTEUR=OPER(nom='CONDUCTEUR',op=None,sd_prod=conducteur,UIinfo = { "groupes" : ( "CACHE", ) },
-                Conductivite=SIMP(statut='o',typ='R',fr='en S/m',ang='en S/m'),
+                Conductivite=SIMP(statut='o',typ='R',fr='en S/m',ang='(S/m)'),
                 PermeabiliteRelative=SIMP(statut='o',typ='R',),
 )
 NOCOND=OPER(nom='NOCOND',op=None,sd_prod=nocond,UIinfo = { "groupes" : ( "CACHE", ) },
@@ -53,12 +63,12 @@ VCUT=OPER(nom='VCUT',op=None,sd_prod=vcut,UIinfo = { "groupes" : ( "CACHE", ) },
 )
 ZS=OPER(nom='ZS',op=None,sd_prod=zs,UIinfo = { "groupes" : ( "CACHE", ) },
                 Conductivite=SIMP(statut='o',typ='R',),
-                PermeabiliteRelative=SIMP(statut='o',typ='R',),
+                Permeabilite=SIMP(statut='o',typ='R',),
 )
 PARAMETRES=PROC(nom='PARAMETRES',op=None, UIinfo = { "groupes" : ( "CACHE", ) },
              RepCarmel=SIMP(typ='Repertoire',fr= "Repertoire Carmel",ang= "Carmel Directory",statut= "o",defaut="/projets/projets.002/carmel3d.001/frequentiel/V_240/Compil"),
              TypedeFormule=SIMP(statut='o',typ='TXM',into=("TOMEGA","APHI")),
-             Frequence=SIMP(statut='o',typ='I',fr="en Hz",ang="en Hz"),
-             Nb_Max_Iterations=SIMP(statut='o',typ='I',val_min=1,val_max=10000,defaut=10000),
+             Frequence=SIMP(statut='o',typ='I',fr="en Hz",ang="(Hz)"),
+             Nb_Max_Iterations=SIMP(statut='o',typ='I',val_min=1,val_max=50000,defaut=10000),
              Erreur_Max=SIMP(statut='o',typ='R',defaut=1E-9),
 )
