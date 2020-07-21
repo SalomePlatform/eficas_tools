7a00190da5b2bcfc536f1bdfcfde113e427f0155
pascale.noyret <pascale.noyret@edf.fr>
2020-07-21 15:42:36 +0200

    et test telemac

---
+++ MT/images/dicoDesIcones.py
@@ -0,0 +1,8 @@
+dicoIcones={ 
+ 'MATERIAUX' : "/home/A96028/QT5GitEficasTravail/eficas/MT/images/443-3.jpg",
+ 'ZONE'      : "/home/A96028/QT5GitEficasTravail/eficas/MT/images/Bouquet_PN.jpg",
+ 'PALIER'    : "/home/A96028/QT5GitEficasTravail/eficas/MT/images/cymbidium-atropurpureum-f2.jpg",
+ 'PALIER_TORSION' : "/home/A96028/QT5GitEficasTravail/eficas/MT/images/images.jpeg",
+ 'BUTEE'   : "/home/A96028/QT5GitEficasTravail/eficas/MT/images/rose.JPG",
+}
+dicoImages={}
