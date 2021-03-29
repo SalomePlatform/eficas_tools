b40366c908695962ba185fb4487e57143bf8927c
pascale.noyret <pascale.noyret@edf.fr>
2021-03-29 15:11:55 +0200

    chgt copyright

--- InterfaceQT4/eficas_go.py
+++ InterfaceQT4/eficas_go.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) 2007-2017   EDF R&D
+# Copyright (C) 2007-2021   EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -25,6 +25,10 @@ from __future__ import print_function
 try :
    from builtins import str
 except : pass
+print ('yyyyyyyyyyyyyy')
+print ('yyyyyyyyyyyyyy')
+print ('yyyyyyyyyyyyyy')
+print ('yyyyyyyyyyyyyy')
 
 
 import sys,os
--- Validation/V_PROC_ETAPE.py
+++ Validation/V_PROC_ETAPE.py
@@ -1,7 +1,6 @@
 # coding=utf-8
-# person_in_charge: mathieu.courtois at edf.fr
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2017  EDF R&D                  WWW.CODE-ASTER.ORG
+# Copyright (C) 2007-2021   EDF R&D
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
