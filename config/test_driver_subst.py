68bb183d43025ba63d4d3a26f4c23985bcd113a3
pascale.noyret <pascale.noyret@edf.fr>
2021-09-20 16:43:17 +0200

    ajout config

---
+++ config/test_driver_subst.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+
+#print(sys.argv[:])
+
+import @module@ as mdm
+import pyxb
+
+#THESE CONFIGURATION LINES ARE FOR ADVANCED INTERNAL TEST ONLY, YOU DON'T NEED TO CONFIGURE PYXB LIKE THIS  
+#mdm.pyxb.GlobalValidationConfig._setContentInfluencesGeneration(mdm.pyxb.GlobalValidationConfig.NEVER)
+mdm.pyxb.GlobalValidationConfig._setContentInfluencesGeneration(mdm.pyxb.GlobalValidationConfig.ALWAYS)
+mdm.pyxb.GlobalValidationConfig._setInvalidElementInContent(mdm.pyxb.GlobalValidationConfig.RAISE_EXCEPTION)
+mdm.pyxb.GlobalValidationConfig._setOrphanElementInContent(mdm.pyxb.GlobalValidationConfig.RAISE_EXCEPTION)
+
+o1 = mdm.CreateFromDocument(open('@file@').read())
+print(o1.toDOM().toprettyxml())
+
